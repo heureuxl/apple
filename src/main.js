023a00a5332606d22e508b3a860761fbc090edc8
tjustb17515614 <485632315@qq.com>
2020-05-23 15:26:49 +0800

    首页完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+import './assets/css/global.css'
 
 Vue.config.productionTip = false
+
+/* 路由发生变化修改页面title */
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  next()
+})
 
 new Vue({
   router,
---
+++ src/plugins/element.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import { Button,Link } from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Link)
